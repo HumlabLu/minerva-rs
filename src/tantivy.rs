8c3a278396cf0a30b553e4e5795e2030bad80ac3
Peter Berck <peter.berck@humlab.lu.se>
2024-07-03 07:17:47 +0200

    Calculation of hash in insert_doc() function.

--- src/tantivy.rs
+++ src/tantivy.rs
@@ -3,9 +3,8 @@ use tantivy::query::{QueryParser, TermQuery};
 use tantivy::schema::*;
 use tantivy::{doc, Index, IndexReader, IndexWriter, ReloadPolicy};
 use tantivy::directory::MmapDirectory;
-use tempfile::TempDir;
 use std::path::Path;
-use tantivy::snippet::{Snippet, SnippetGenerator};
+use tantivy::snippet::{SnippetGenerator};
 use once_cell::sync::Lazy;
 
 static SCHEMA: Lazy<Schema> = Lazy::new(|| {
@@ -51,9 +50,22 @@ pub fn insert_document(index: &Index, title: &str, body: &str, page_number: u64,
 }
 
 // Takes ownership of the doc!
-pub fn insert_doc(index: &Index, tdoc: TantivyDocument) -> tantivy::Result<()> {
+pub fn insert_doc(index: &Index, mut tdoc: TantivyDocument) -> tantivy::Result<()> {
     let mut index_writer: IndexWriter = index.writer(50_000_000)?;
 
+    // Need to calculate the body_hash.
+    let schema = index.schema();
+    let body_field = schema.get_field("body").unwrap();
+    let hash_body_field = schema.get_field("hash_body").unwrap();
+    /*for x in tdoc.get_all(body_field) {
+        println!("{:?}", x);
+    }*/
+    if let Some(x) = tdoc.get_first(body_field) {
+        let hash_body_value = x.as_str().unwrap();
+        let hash_body_value = blake3::hash(hash_body_value.as_bytes());
+        tdoc.add_text(hash_body_field, hash_body_value);
+        //println!("{:?}", hash_body_value);
+    }
     index_writer.add_document(tdoc)?;
     index_writer.commit()?;
     
@@ -64,6 +76,7 @@ pub fn insert_doc(index: &Index, tdoc: TantivyDocument) -> tantivy::Result<()> {
 // Probably not on an Index but on an IndexWriter so we check before
 // inserting in a loop?
 // unique_id needs to be a hash on the title+body, or something.
+// Adapted from https://github.com/quickwit-oss/tantivy/blob/main/examples/deleting_updating_documents.rs
 pub fn document_exists(index: &Index, hash_body: &str) -> tantivy::Result<bool> {
     let reader: IndexReader = index.reader_builder().reload_policy(ReloadPolicy::Manual).try_into()?;
     let schema = index.schema();
@@ -100,7 +113,6 @@ pub fn tanttest() -> tantivy::Result<()> {
     insert_document(&index, "Another title with Mice", "Example body text \
 Longer string.", 1, 1)?;
 
-    let mut index_writer: IndexWriter = index.writer(50_000_000)?; // 50 MB buffer for indexing.
     let title = schema.get_field("title").unwrap();
     println!("title: {:?}", title);
     let body = schema.get_field("body").unwrap();
@@ -118,8 +130,9 @@ Longer string.", 1, 1)?;
          eighty-four days now without taking a fish.",
     );
     old_man_doc.add_u64(page_number, 28);
-    index_writer.add_document(old_man_doc)?;
-    //insert_doc(&index, old_man_doc).unwrap(); // nope, we already have an active index_writer.
+    insert_doc(&index, old_man_doc).unwrap();
+
+    let mut index_writer: IndexWriter = index.writer(50_000_000)?; // 50 MB buffer for indexing.
     
     index_writer.add_document(doc!(
         title => "Of Mice and Men",
@@ -179,7 +192,7 @@ Longer string.", 1, 1)?;
     }
     let query = query_parser.parse_query("title:sea^20 body:whale^70")?;
 
-    let (_score, doc_address) = searcher
+    let (_score, _doc_address) = searcher
         .search(&query, &TopDocs::with_limit(1))?
         .into_iter()
         .next()
